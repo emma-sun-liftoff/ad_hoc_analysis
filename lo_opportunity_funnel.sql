48b8d5e3b8ebb1efda7c849c7295c3601c3c218a
Emma Sun <97474101+emma-sun-liftoff@users.noreply.github.com>
2023-02-21 11:32:38 -0500

    Update lo_opportunity_funnel.sql
    unattributed funnel at the device level

--- lo_opportunity_funnel.sql
+++ lo_opportunity_funnel.sql
@@ -164,6 +164,92 @@ sum(total_bids) AS total_bids
 FROM funnel
 
 
+-- unattribution funnel at the device level
+WITH temp AS (SELECT 
+device_id_sha1 AS device_id
+, is_uncredited 
+, CASE WHEN try(IF(from_big_endian_64(reverse(substr(from_hex(device_id_sha1), 1, 8))) >= 0,
+  CAST(from_big_endian_64(reverse(substr(from_hex(device_id_sha1), 1, 8))) AS double precision) / power(2, 64), 
+  (CAST(from_big_endian_64(reverse(substr(from_hex(device_id_sha1), 1, 8))) AS double precision) / power(2, 64))+1)) < 0.000125 THEN 'yes' ELSE 'no' END AS in_no_bid_user_sample
+, CASE WHEN try(IF(from_big_endian_64(reverse(substr(from_hex(device_id_sha1), 1, 8))) >= 0,
+  CAST(from_big_endian_64(reverse(substr(from_hex(device_id_sha1), 1, 8))) AS double precision) / power(2, 64), 
+  (CAST(from_big_endian_64(reverse(substr(from_hex(device_id_sha1), 1, 8))) AS double precision) / power(2, 64))+1)) < 0.0002   THEN 'yes' ELSE 'no' END AS in_bid_user_sample
+FROM rtb.raw_installs 
+WHERE  date_diff('hour', from_iso8601_timestamp(dt), current_date) <= 24
+	AND app_id = 1579)
+
+
+, devices AS (SELECT 
+DISTINCT device_id
+FROM temp
+WHERE in_bid_user_sample = 'yes' AND in_no_bid_user_sample = 'yes'
+AND is_uncredited = TRUE
+)
+
+
+
+, funnel AS (
+SELECT 
+bid_request__device__platform_specific_id_sha1 AS device_id
+, count(*) AS total_bids 
+, 0 AS total_no_bids
+, 0 AS impressions 
+, 0 AS installs 
+FROM rtb.user_sampled_bids 
+WHERE bid_request__device__platform_specific_id_sha1 IN (SELECT device_id FROM devices)
+AND date_diff('hour', from_iso8601_timestamp(dt), current_date) <= 24
+GROUP BY 1
+
+UNION ALL 
+SELECT 
+bid_request__device__platform_specific_id_sha1 AS device_id
+, 0 AS total_bids 
+, count(*) AS total_no_bids
+, 0 AS impressions 
+, 0 AS installs 
+FROM rtb.user_sampled_no_bids 
+WHERE bid_request__device__platform_specific_id_sha1 IN (SELECT device_id FROM devices)
+AND date_diff('hour', from_iso8601_timestamp(dt), current_date) <= 24
+GROUP BY 1
+
+UNION ALL 
+SELECT
+bid__bid_request__device__platform_specific_id_sha1 AS device_id
+, 0 AS total_bids 
+, 0 AS total_no_bids 
+, count(*) AS impressions
+, 0 AS installs 
+FROM rtb.impressions_with_bids 
+WHERE bid__bid_request__device__platform_specific_id_sha1 IN (SELECT device_id FROM devices)
+AND date_diff('hour', from_iso8601_timestamp(dt), current_date) <= 24
+GROUP BY 1
+
+UNION ALL 
+SELECT 
+ad_click__impression__bid__bid_request__device__platform_specific_id_sha1 AS device_id
+, 0 AS total_bids 
+, 0 AS total_no_bids 
+, 0 AS impressions 
+, count(*) AS installs 
+FROM rtb.installs 
+WHERE ad_click__impression__bid__bid_request__device__platform_specific_id_sha1 IN (SELECT device_id FROM devices)
+AND date_diff('hour', from_iso8601_timestamp(dt), current_date) <= 24
+GROUP BY 1
+
+)
+
+
+
+SELECT
+device_id
+, sum(total_bids) AS total_bids
+, sum(total_no_bids) AS total_no_bids
+, sum(impressions) AS impressions
+, sum(installs) AS installs
+FROM funnel
+GROUP BY 1
+
+
 
 -- [To validate if see bid requests] 
 
