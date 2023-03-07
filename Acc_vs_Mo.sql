28e47968e588f3a76be34a1c82c9ae6316993cd1
Emma Sun <97474101+emma-sun-liftoff@users.noreply.github.com>
2023-03-07 12:17:23 -0500

    Update Acc_vs_Mo.sql

--- Acc_vs_Mo.sql
+++ Acc_vs_Mo.sql
@@ -203,3 +203,19 @@ from dev_counts
 left join medians using(winning_bid_bundle, imp_week)
 group by 1,2,3
 order by 1,2
+
+-- device level win rate
+
+select winning_bid_bundle, 
+ count(dev_id_at_impression) as total_user
+  , sum(case when winner_id in ( '60f0c4687a479f0016f763e2', '60f0c55d8b63dd00161567a6', '5bc0e200eb95ad0016cfb668', '5f86383c18eece0010058e72') then 1 else null end) as mo_impressions
+  , sum(case when winner_id in ('6279c195e1e538a5d1e64090', '627afba9cc8bd7121ff60426', '627afcad6763d32d545b71d7', '6189c34a478cdcadb14300f8', '61c276a2c0b8ef544ed8d9c1', '61c27759c0b8ef544ed8d9c3', '61c277c0d3819d69caeeba19', '5f6415539290720015d69d84', '5f6415d4fd2311001528e9ef', '5f74d16d466598000fe30644', '5f7b8e8efeb062001649e792') then 1 else null end) as lo_impressions
+from edsp_transactions
+where
+   timestamp_at_impression >= getdate() - interval '28 days'
+  and dev_platform = 'android'
+  --and geoip_country_code_at_impression = 'US'
+  --and adomain in ('king.com', 'playrix.com')
+  and (winning_bid_bundle ILIKE '%.playrix.%' OR winning_bid_bundle ILIKE '%.king.%')
+  --or winning_bid_bundle ILIKE '%.playrix.%')
+group by 1
